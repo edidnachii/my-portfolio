4d532df0a76b56bc06190c4daf0218ed61fbc8a7
edidnachii <aidxxd.dan11@gmail.com>
2025-05-21 15:51:19 +0800

    Initial portfolio commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello, I'm [Your Name]</h1>
+      <p>I'm an aspiring web developer.</p>
+      <a href="mailto:you@example.com">Contact Me</a>
+    </div>
+  );
+}
+
+export default App;
+
